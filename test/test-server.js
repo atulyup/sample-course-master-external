b86492ee617ef37189fd4e742824c3b8c337b16e
pradeep <raju30.wls@gmail.com>
2020-07-31 16:24:22 +0530

    Updated test.js

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to [TEAM NAME'S] application</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>Welcome to <b>Cloud Chronicles</b> application</h1>"));
         return done();
       });
 
